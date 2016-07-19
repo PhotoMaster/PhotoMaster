1fa61332acda9d5bf7ac94848208f973fd435655
Qiong Wu <qiowu@microsoft.com>
2016-07-19 18:15:35 +0800

    PhotoDetailPage

---
+++ PhotoMaster/DetailView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using PhotoMaster.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PhotoMaster
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DetailView : Page
+    {
+        public DetailView()
+        {
+            this.InitializeComponent();
+
+            m_photo = new Photo();
+
+            // Test data
+            m_photo.PhotoImage = new BitmapImage(new Uri("ms-appx:/Assets/index_bg.png"));
+            m_photo.PhotoDescription = "This image is a test image, XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+            this.image.Source = m_photo.PhotoImage;
+            this.detail.Text = m_photo.PhotoDescription;
+        }
+
+        private Photo m_photo;
+    }
+}
---
+++ PhotoMaster/Model/Photo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace PhotoMaster.Model
+{
+    public class Photo
+    {
+        public int PhotoId;
+        public BitmapImage PhotoImage;
+        public Geoposition PhotoGPS;
+        public String PhotoDescription;
+        public bool PhotoIsSelected;
+    }
+}
