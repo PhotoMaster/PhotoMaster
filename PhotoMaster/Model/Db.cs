60dfc562064acbfc74834f5b8d83dd4fef54dec6
Jiqian Zhang (PERSON CONSULTING) <t-jiqzha@microsoft.com>
2016-07-21 10:19:03 +0800

    add small photo to show in map properly

--- PhotoMaster/Model/Db.cs
+++ PhotoMaster/Model/Db.cs
@@ -18,49 +18,49 @@ namespace PhotoMaster.Model
             BitmapImage image = new BitmapImage(new Uri("ms-appx:/Assets/photos/040.jpg"));
             BasicGeoposition position = new BasicGeoposition() { Latitude = 39.990712, Longitude = 116.27941 };
             Geopoint gpsPoint = new Geopoint(position);
-            Photo photo = new Photo(40, image, gpsPoint, 90, "ms-appx:///Assets/photos/040.jpg");
+            Photo photo = new Photo(40, image, gpsPoint, 90, "ms-appx:///Assets/photos/040-small.jpg");
             photos.Add(gpsPoint, photo);
 
             image = new BitmapImage(new Uri("ms-appx:/Assets/photos/018.jpg"));
             position = new BasicGeoposition() { Latitude = 39.997699, Longitude = 116.272619 };
             gpsPoint = new Geopoint(position);
-            photo = new Photo(18, image, gpsPoint, 90, "ms-appx:/Assets/photos/018.jpg");
+            photo = new Photo(18, image, gpsPoint, 90, "ms-appx:///Assets/photos/018-small.jpg");
             photos.Add(gpsPoint, photo);
 
             image = new BitmapImage(new Uri("ms-appx:/Assets/photos/060.jpg"));
             position = new BasicGeoposition() { Latitude = 39.986248, Longitude = 116.273786 };
             gpsPoint = new Geopoint(position);
-            photo = new Photo(60, image, gpsPoint, 90, "ms-appx:/Assets/photos/060.jpg");
+            photo = new Photo(60, image, gpsPoint, 90, "ms-appx:///Assets/photos/060-small.jpg");
             photos.Add(gpsPoint, photo);
 
             image = new BitmapImage(new Uri("ms-appx:/Assets/photos/061.jpg"));
             position = new BasicGeoposition() { Latitude = 39.98619, Longitude = 116.273915 };
             gpsPoint = new Geopoint(position);
-            photo = new Photo(61, image, gpsPoint, 90, "ms-appx:/Assets/photos/061.jpg");
+            photo = new Photo(61, image, gpsPoint, 90, "ms-appx:///Assets/photos/061-small.jpg");
             photos.Add(gpsPoint, photo);
 
             image = new BitmapImage(new Uri("ms-appx:/Assets/photos/062.jpg"));
             position = new BasicGeoposition() { Latitude = 39.98626, Longitude = 116.273931 };
             gpsPoint = new Geopoint(position);
-            photo = new Photo(62, image, gpsPoint, 90, "ms-appx:/Assets/photos/062.jpg");
+            photo = new Photo(62, image, gpsPoint, 90, "ms-appx:///Assets/photos/062-small.jpg");
             photos.Add(gpsPoint, photo);
 
             image = new BitmapImage(new Uri("ms-appx:/Assets/photos/054.jpg"));
             position = new BasicGeoposition() { Latitude = 39.983601, Longitude = 116.27355 };
             gpsPoint = new Geopoint(position);
-            photo = new Photo(54, image, gpsPoint, 90, "ms-appx:/Assets/photos/054.jpg");
+            photo = new Photo(54, image, gpsPoint, 90, "ms-appx:///Assets/photos/054-small.jpg");
             photos.Add(gpsPoint, photo);
 
             image = new BitmapImage(new Uri("ms-appx:/Assets/photos/011.jpg"));
             position = new BasicGeoposition() { Latitude = 39.996223, Longitude = 116.26758 };
             gpsPoint = new Geopoint(position);
-            photo = new Photo(11, image, gpsPoint, 90, "ms-appx:/Assets/photos/011.jpg");
+            photo = new Photo(11, image, gpsPoint, 90, "ms-appx:///Assets/photos/011-small.jpg");
             photos.Add(gpsPoint, photo);
 
             image = new BitmapImage(new Uri("ms-appx:/Assets/photos/075.jpg"));
             position = new BasicGeoposition() { Latitude = 39.993428, Longitude = 116.267236 };
             gpsPoint = new Geopoint(position);
-            photo = new Photo(75, image, gpsPoint, 90, "ms-appx:/Assets/photos/075.jpg");
+            photo = new Photo(75, image, gpsPoint, 90, "ms-appx:/Assets/photos/075-small.jpg");
             photos.Add(gpsPoint, photo);
 
 
