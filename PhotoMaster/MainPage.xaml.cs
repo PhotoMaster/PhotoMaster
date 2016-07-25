d27e170127ee9f1533ab9512c862f472a7fe12c1
Jacky Yang <jackyyan@microsoft.com>
2016-07-25 15:15:03 +0800

    Hide status bar

--- PhotoMaster/MainPage.xaml.cs
+++ PhotoMaster/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,6 +28,10 @@ namespace PhotoMaster
         {
             this.InitializeComponent();
 
+            StatusBar statusBar = Windows.UI.ViewManagement.StatusBar.GetForCurrentView();
+
+            // Hide the status bar
+            statusBar.HideAsync();
         }
 
         private void click_map(object sender, TappedRoutedEventArgs e)
